52ffdc5256a1824e36929b09720e86e2331a5f49
Dima <dchaika1990@gmail.com>
2021-03-23 09:33:14 +0200

    end first lesson

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//slider init
 const mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 
@@ -8,4 +9,40 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+//scroll smooth
+const scrollLink = (btns) => {
+	const scrollLinkBtns = document.querySelectorAll(btns);
+
+	scrollLinkBtns.forEach(btn => {
+		btn.addEventListener('click', function (e) {
+			e.preventDefault();
+			const id = btn.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			})
+		})
+	})
+}
+
 // cart
+const cartModal = () => {
+	const buttonCart = document.querySelector('.button-cart');
+	const modalCart = document.querySelector('#modal-cart');
+
+	const openModal = () => modalCart.classList.add('show');
+	const closeModal = () => modalCart.classList.remove('show');
+
+	buttonCart.addEventListener('click', openModal);
+	document.addEventListener('click', function (e) {
+		const elem = e.target;
+		if (elem.classList.contains('overlay') || elem.classList.contains('modal-close')) closeModal()
+	});
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+	cartModal();
+	scrollLink('a.scroll-link');
+})
