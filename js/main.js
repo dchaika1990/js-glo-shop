051f76ab3a582c7c11814ed63284beed5e3787cb
Dima <dchaika1990@gmail.com>
2021-03-23 09:02:40 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper('.swiper-container', {
+	loop: true,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.slider-button-next',
+		prevEl: '.slider-button-prev',
+	},
+});
+
+// cart
